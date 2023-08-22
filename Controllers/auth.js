b7c31048c1ecb3df9d86a6443df46c10c0091c49
Ehtesham Zahid <shamimalick321@gmail.com>
2023-08-22 11:44:01 +0500

    Backend Finished

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -157,6 +157,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   //   "host"
   // )}/api/users/resetPassword/${resetToken}`;
 
+  // RESETURL
   const resetURL = `${req.protocol}://e-commerce-shop-it.vercel.app/resetPassword/${resetToken}`;
 
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL} .\nIf you didn't forget your password, please ignore this email!`;
